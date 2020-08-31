5529f8167bb0a05a2f26eb3f5f6c6488b7cddb66
fran.komlinovic <fran.komlinovic@serengetitech.com>
2020-08-31 19:27:55 +0200

    Removed non generic content

--- src/main/java/hr/brocom/recept/enums/PickupType.java
+++
@@ -1,6 +0,0 @@
-package hr.brocom.recept.enums;
-
-public enum PickupType {
-    SINGLE_COIL,
-    HUMBUCKER;
-}
--- src/main/java/hr/brocom/recept/enums/Position.java
+++
@@ -1,7 +0,0 @@
-package hr.brocom.recept.enums;
-
-public enum Position {
-    BRIDGE,
-    MIDDLE,
-    NECK
-}
